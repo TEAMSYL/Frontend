5a14f4ac43395b3962bc28c4e54f26124b730104
JeonghakLee <wjdgkr0527@naver.com>
2022-07-30 15:32:29 +0900

    fix: 카테고리 버튼 색 추가

--- src/components/Category/CategoryGrid.js
+++ src/components/Category/CategoryGrid.js
@@ -18,6 +18,16 @@ import ParkIcon from "@mui/icons-material/Park";
 import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
 import { Typography } from "@mui/material";
 import productApi from "../../api/Product.tsx";
+import {
+  orange,
+  grey,
+  green,
+  red,
+  lightBlue,
+  deepPurple,
+  yellow,
+  brown,
+} from "@mui/material/colors";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -36,72 +46,73 @@ export default function CategoryGrid(props) {
     productApi.getCategoryProducts(id);
     navigate("/");
   };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(1, e)}>
-            <ComputerIcon />
+            <ComputerIcon sx={{ color: grey[400] }} />
             <Typography sx={{ fontSize: 8 }}>디지털기기</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(2, e)}>
-            <ChairIcon />
+            <ChairIcon sx={{ color: brown[500] }} />
             <Typography sx={{ fontSize: 8 }}>가구</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(3, e)}>
-            <BookIcon />
+            <BookIcon sx={{ color: orange[400] }} />
             <Typography sx={{ fontSize: 8 }}>책</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(4, e)}>
-            <ConfirmationNumberIcon />
+            <ConfirmationNumberIcon sx={{ color: red[600] }} />
             <Typography sx={{ fontSize: 8 }}>티켓/음반</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(5, e)}>
-            <CheckroomIcon />
+            <CheckroomIcon sx={{ color: grey[900] }} />
             <Typography sx={{ fontSize: 8 }}>의류</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(6, e)}>
-            <SportsTennisIcon />
+            <SportsTennisIcon sx={{ color: green[400] }} />
             <Typography sx={{ fontSize: 8 }}>스포츠용품</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(7, e)}>
-            <PetsIcon />
+            <PetsIcon sx={{ color: brown[900] }} />
             <Typography sx={{ fontSize: 8 }}>반려동물용품</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(8, e)}>
-            <AddShoppingCartIcon />
+            <AddShoppingCartIcon color="primary" />
             <Typography sx={{ fontSize: 8 }}>생활용품</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(9, e)}>
-            <ParkIcon />
+            <ParkIcon sx={{ color: green[900] }} />
             <Typography sx={{ fontSize: 8 }}>식물</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(10, e)}>
-            <AutoFixHighIcon />
+            <AutoFixHighIcon sx={{ color: yellow[200] }} />
             <Typography sx={{ fontSize: 8 }}>뷰티/미용</Typography>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item onClick={(e) => handleOnclick(11, e)}>
-            <SportsEsportsIcon />
+            <SportsEsportsIcon sx={{ color: red[400] }} />
             <Typography sx={{ fontSize: 8 }}>게임</Typography>
           </Item>
         </Grid>
